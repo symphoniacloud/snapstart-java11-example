f2cfe3d81193b901c2c73cbb83e168340de3ff73
Mike Roberts <mike@symphonia.io>
2023-01-12 16:37:08 -0500

    Add logging to see timings of each statement in handler function

--- src/main/java/book/api/WeatherQueryLambda.java
+++ src/main/java/book/api/WeatherQueryLambda.java
@@ -18,16 +18,21 @@ public class WeatherQueryLambda {
     private static final String DEFAULT_LIMIT = "50";
 
     public ApiGatewayResponse handler(ApiGatewayRequest request) throws IOException {
+        System.out.println("** 1 **");
         final String limitParam = request.queryStringParameters == null
                 ? DEFAULT_LIMIT
                 : request.queryStringParameters.getOrDefault("limit", DEFAULT_LIMIT);
+        System.out.println("** 2 **");
         final int limit = Integer.parseInt(limitParam);
 
+        System.out.println("** 3 **");
         final ScanRequest scanRequest = new ScanRequest()
                 .withTableName(tableName)
                 .withLimit(limit);
+        System.out.println("** 4 **");
         final ScanResult scanResult = dynamoDB.scan(scanRequest);
 
+        System.out.println("** 5 **");
         final List<WeatherEvent> events = scanResult.getItems().stream()
                 .map(item -> new WeatherEvent(
                         item.get("locationName").getS(),
@@ -38,8 +43,10 @@ public class WeatherQueryLambda {
                 ))
                 .collect(Collectors.toList());
 
+        System.out.println("** 6 **");
         final String json = objectMapper.writeValueAsString(events);
 
+        System.out.println("** 7 **");
         return new ApiGatewayResponse(200, json);
     }
 }
